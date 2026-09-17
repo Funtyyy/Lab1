c89f7008eb81d98e82260d0a73c3835eef87a1f8
Funtyyy <ivankolyutiy@gmail.com>
2026-09-17 20:54:00 +0300

    first commit

---
+++ lab1.py
@@ -0,0 +1,28 @@
+game = "God of war"
+print(f"моя улбюлена гра - {game}")
+
+
+
+is_student = False
+is_teacher = True
+print("is_student and is_teacher:", is_student and is_teacher)
+print ("is_student or is_teacher:", is_student or is_teacher)
+print("not is_student:", not is_student)
+
+
+
+
+is_raining = False 
+is_snowing = True
+print("not is_snowing:", not is_snowing)
+print("not is_raining:", not is_raining)
+
+
+
+
+import math
+
+def add(x, y):
+    return (x*y + x**2*y**2) / math.tan(x**5 * y**5) - math.cos(x*y) + x * y**(5/6) + 173.11*x
+
+(add(2.134, 0.129))
